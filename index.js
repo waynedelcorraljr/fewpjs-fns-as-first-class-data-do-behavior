a780e6bfa078f556e3e5ff60b6af5bd9fda4b215
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-02-05 22:46:48 -0600

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let timeArr = timeString.split(':');
+  let hour = parseInt(timeArr[0], 10) * 100;
+  let minutes = parseInt(timeArr[1], 10);
+  let timeInt = hour + minutes
+  if (timeInt < 1200) {
+    return "Good Morning"
+  } else if (1200 < timeInt && timeInt < 1700) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  let node = document.querySelector("#greeting")
+  node.innerText = string
+}
